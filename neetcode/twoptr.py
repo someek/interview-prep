187431a906722c11ccabf3a11a8b58589920bb19
Soumik Chatterjee <soumik-chatterjee@proton.me>
2026-02-23 22:27:10 +0530

    feat(neetcode): solve 'Two Sum II - Input Array Is Sorted'

--- neetcode/twoptr.py
+++ neetcode/twoptr.py
@@ -1,6 +1,28 @@
 import doctest
 
 
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+def two_sum(nums: list[int], target: int) -> list[int]:
+    """
+    >>> two_sum([2,7,11,15], 9)
+    [1, 2]
+    >>> two_sum([2,3,4], 6)
+    [1, 3]
+    >>> two_sum([-1,0], -1)
+    [1, 2]
+    """
+    start, end = 0, len(nums) - 1
+    while start < end:
+        sum = nums[start] + nums[end]
+        if sum < target:
+            start += 1
+        elif sum > target:
+            end -= 1
+        else:
+            return [start + 1, end + 1]
+    raise Exception("no solution found")
+
+
 # https://leetcode.com/problems/valid-palindrome
 def is_palindrome(s: str) -> bool:
     """
